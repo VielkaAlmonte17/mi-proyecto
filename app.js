2b79967f5011df6953bb5747956a32a47c49fe13
VielkaAlmonte17 <114878254+VielkaAlmonte17@users.noreply.github.com>
2025-02-13 20:42:32 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function HolaMundo() {
+    return "Hola Mundo";
+  }
+  
+  module.exports = HolaMundo;
+//Vielka Leticia Almonte De la Cruz 2022-1784
+
---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+const HolaMundo = require("../app");  
+
+test("Debe retornar 'Hola Mundo'", () => {
+  expect(HolaMundo()).toBe("Hola Mundo");
+});
+
+//Vielka Leticia Almonte De la Cruz 2022-1784
+
